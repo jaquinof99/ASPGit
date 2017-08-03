63429380231a5860383db7602d1e38d5233743aa
Your Name <you@example.com>
2017-08-02 18:08:43 -0600

    asp.net chat

---
+++ SignalRChat/SignalRChat/App_Start/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(SignalRChat.App_Start.Startup))]
+
+namespace SignalRChat.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
---
+++ SignalRChat/SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static class UserHandler
+        {
+            public static HashSet<string> ConnectedIds = new HashSet<string>();
+        }
+
+        public class MyHub : Hub
+        {
+            public override Task OnConnected()
+            {
+                UserHandler.ConnectedIds.Add(Context.ConnectionId);
+                return base.OnConnected();
+            }
+
+            public override Task OnDisconnected(bool stopCalled)
+            {
+                UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+                return base.OnDisconnected(stopCalled);
+            }
+        }
+        public void Send(string name, string message, string timenow)
+        {
+            timenow = DateTime.Now.ToString();
+            //Call the addNewMEssageToPage method to update clients
+            Clients.All.addNewMessageToPage(name, message, timenow);
+        }
+    }
+}
